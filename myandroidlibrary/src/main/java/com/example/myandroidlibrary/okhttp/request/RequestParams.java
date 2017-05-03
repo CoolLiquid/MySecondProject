e2bb9c5b27db6539ee67ecdfe5c44c73e3bbc1d5
weinp <861938262@qq.com>
2017-05-03 16:38:18 +0800

    第二次封装requestparams

--- myandroidlibrary/src/main/java/com/example/myandroidlibrary/okhttp/request/RequestParams.java
+++ myandroidlibrary/src/main/java/com/example/myandroidlibrary/okhttp/request/RequestParams.java
@@ -4,6 +4,7 @@ package com.example.myandroidlibrary.okhttp.request;
  * Created by weinp on 2017/5/3.
  */
 
+import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -15,26 +16,33 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 
 public class RequestParams {
-    public ConcurrentHashMap<String , String> urlParams = new ConcurrentHashMap<>();
-    public ConcurrentHashMap<String , Object> fileParams = new ConcurrentHashMap<>();
+    public ConcurrentHashMap<String, String> urlParams = new ConcurrentHashMap<>();
+    public ConcurrentHashMap<String, Object> fileParams = new ConcurrentHashMap<>();
 
 
-    public RequestParams(){this((Map<String , String >) null);}
-    /**
-     * @param source the source key/value string map to add.
-     */
-    public RequestParams(Map<String , String > source){
-
+    public RequestParams() {
+        this((Map<String, String>) null);
     }
 
     /**
-     *
+     * @param source the source key/value string map to add.
+     */
+    public RequestParams(Map<String, String> source) {
+        if (source != null) {
+            for (Map.Entry<String, String> entry :
+                    source.entrySet()) {
+                put(entry.getKey(), entry.getValue());
+            }
+        }
+    }
+
+    /**
      * @param key
      * @param value
      */
-    public RequestParams(final String key , final String value){
+    public RequestParams(final String key, final String value) {
 
-        this(new HashMap<String, String>(){
+        this(new HashMap<String, String>() {
             {
                 put(key, value);
             }
@@ -42,29 +50,27 @@ public class RequestParams {
     }
 
     /**
-     *
      * @param key
      * @param value
      */
-    public void put(String key , String value){
-        if(key!=null&&value != null){
-            urlParams.put(key , value);
+    private void put(String key, String value) {
+        if (key != null && value != null) {
+            urlParams.put(key, value);
         }
     }
 
     /**
-     *
      * @param key
      * @param value
      */
-    public void put(String key , Objects value){
-        if(key != null && value!=null){
-            fileParams.put(key , value);
+    private void put(String key, Object value) throws FileNotFoundException {
+        if (key != null && value != null) {
+            fileParams.put(key, value);
         }
     }
 
-    public boolean hasParams(){
-        if(urlParams.size() > 0 || fileParams.size() > 0 ){
+    public boolean hasParams() {
+        if (urlParams.size() > 0 || fileParams.size() > 0) {
             return true;
         }
         return false;
